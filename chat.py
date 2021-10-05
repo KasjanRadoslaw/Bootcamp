4271146edfce9da2fc056e09636790d88c00a762
KasjanRadoslaw <91878230+KasjanRadoslaw@users.noreply.github.com>
2021-10-05 14:24:49 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,21 +1,26 @@
+# Frage nach dem Namen.
 name = input("Wie heißt du? ")
 print(f"{name.title()}, was ein toller Name!\n")
+
+# Frage nach dem Alter mit Kommentar.
 age = input("Wie alt bist du? ")
 if int(age) < 20:
 	print("Du bist noch ein Jugendlicher!\n")
 else:
 	print("Du bist schon erwachsen!\n")
+	
+# Frage mit 2 Antwortoptionen.
 question = input("Magst du eigentlich Hunde mehr als Katzen? ")
 if question.lower() == "ja":
 	print("Ich auch!\n")
 if question.lower() == "nein":
 	print("Du bist also eher ein Katzenmensch!\n")
-prompt = "Hast du noch Fragen an mich?"
-prompt += "\nTippe 'name', um den Namen zu erfahren."
-prompt += "\nSchreibe was anderes, um das Gespräch zu beenden. "
+	
+# Nach Namen Fragen.
+prompt = "Hast du noch Fragen an mich?\n"
+prompt += "Du kannst nach meinem Namen fragen. "
 question2 = input(prompt)
-if question2.lower() == 'name':
+if 'name' in question2.lower():
 	print("Mein Name lautet Ada!")
 else:
 	print("\nVielen Dank für das Gespräch!")
-
