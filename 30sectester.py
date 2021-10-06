247334323b5da96d1c30ab324940c9a6feab2e0f
KasjanRadoslaw <91878230+KasjanRadoslaw@users.noreply.github.com>
2021-10-06 15:13:10 +0200

    Add files via upload

---
+++ 30sectester.py
@@ -0,0 +1,16 @@
+import time
+
+print("Was war das größte Abenteuer meines Lebens?")
+print("Was macht einen Tag zu einem guten Tag?")
+print("Welchen Moment deines Lebens möchtest du am liebsten einrahmen, weil er so schön war?")
+print("Welche Spuren hast du bisher hinterlassen?")
+print("Wo fühlst du dich zu Hause und geborgen?")
+print("Was bringt dich zum Lachen?")
+print("Was ist dein schönster Tagtraum?")
+print("Welche Kritik in deinem Leben hat dich so richtig weitergebracht?")
+print("Wenn du auf dein bisheriges Leben schaust, was erfüllt dich mit Stolz?")
+print("Würdest du gerne noch einmal zur Welt kommen?")
+
+time.sleep(30)
+
+print("Ende der Beantwortungszeit!")
