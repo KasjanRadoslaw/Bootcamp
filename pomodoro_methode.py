2ae5c6df58bce5a86ce10522d6a11c93a260ddb8
KasjanRadoslaw <91878230+KasjanRadoslaw@users.noreply.github.com>
2021-10-07 11:00:44 +0200

    Update pomodoro_methode.py

--- pomodoro_methode.py
+++ pomodoro_methode.py
@@ -1,20 +1,22 @@
-import webbrowser
-import time
+import webbrowser, time
 
-def pomodoro_method(rhythms, rhythm_length):
+def pomodoro_method(rhythms, rhythm_length, break_length):
 	'''Gebe bei 'rhythms' die Anzahl der Rhythmen ein!'''
-	'''GEbe bei 'rhythm_length' die Anzahl der Länge ein!'''
+	'''Gebe bei 'rhythm_length' die Anzahl der Länge ein!'''
+	'''Gebe bei 'break_length' die Länge der Pausen ein!'''
 
 	repeats = 0
 
 	work_number = float(rhythm_length)*60
+	break_number = float(break_length)*60
 
 	while repeats < int(rhythms):
 		time.sleep(work_number)
 		print("Zeit für eine Pause!")
 		webbrowser.open('https://www.youtube.com/watch?v=UceaB4D0jpo')
-		time.sleep(450)
-		print("Zeit zum Weiterarbeiten!\n")
+		time.sleep(break_number)
+		print("Zeit zum Weiterarbeiten!")
 		repeats += 1
+		print(f"Das war deine {repeats} Pause!")
 
 	print("Ende!")
